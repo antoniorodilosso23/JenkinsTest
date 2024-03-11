b535d54716b41b4551e34f1b59678d5f46e05496
antrod <rodilossoa@gmail.com>
2024-03-11 13:21:29 +0100

    Merge remote-tracking branch 'origin/master'

--- .idea/Main.py
+++ .idea/Main.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 def stampaDiTest():
     timestamp = datetime.now().strftime('%H:%M')  # Ottieni l'ora corrente e formatta come HH:MM
-    return f"TestJenkins_ore{timestamp}"
+    return f"TestJenkins_timestamp{timestamp}"
